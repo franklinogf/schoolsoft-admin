f87e720988c3f75efbcb29b148903c9f3a8f2883
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-10-14 12:27:34 -0400

    name to be always lowercase

--- app/Models/Enviroment.php
+++ app/Models/Enviroment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,11 @@ class Enviroment extends Model
     protected $keyType = 'string';
 
     public $timestamps = false;
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => strtolower($value),
+        );
+    }
 }
